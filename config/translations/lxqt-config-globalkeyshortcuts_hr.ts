1a2ceae2442bf03646c327cc7afbfa35d20e5e03
Deepin Developer <deepin-dev@deepin.org>
2022-10-12 13:21:18 +0800

    feat: Init commit
    init lxqt-globalkeys
    
    Log:

---
+++ config/translations/lxqt-config-globalkeyshortcuts_hr.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>DefaultModel</name>
+    <message>
+        <location filename="../default_model.cpp" line="51"/>
+        <source>Command</source>
+        <translation>Naredba</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="52"/>
+        <source>DBus call</source>
+        <translation>DBus poziv</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="53"/>
+        <source>Client</source>
+        <translation>Klijent</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="143"/>
+        <source>Id</source>
+        <translation>Id</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="146"/>
+        <source>Shortcut</source>
+        <translation>Prečac</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="149"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="152"/>
+        <source>Type</source>
+        <translation>Vrsta</translation>
+    </message>
+    <message>
+        <location filename="../default_model.cpp" line="155"/>
+        <source>Info</source>
+        <translation>Informacije</translation>
+    </message>
+</context>
+<context>
+    <name>EditActionDialog</name>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="14"/>
+        <source>Edit Action</source>
+        <translation>Uredi radnju</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="31"/>
+        <source>&amp;Shortcut:</source>
+        <translation>&amp;Prečac:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="74"/>
+        <source>&amp;Description:</source>
+        <translation>&amp;Opis:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="87"/>
+        <source>&amp;Enabled</source>
+        <translation>&amp;Aktivirano</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="102"/>
+        <source>&amp;Command</source>
+        <translation>&amp;Naredba</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="109"/>
+        <source>&amp;DBus message</source>
+        <translation>&amp;DBus poruka</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="118"/>
+        <source>Type:</source>
+        <translation>Vrsta:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="164"/>
+        <source>Co&amp;mmand:</source>
+        <translation>Nared&amp;ba:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="206"/>
+        <source>S&amp;ervice:</source>
+        <translation>Us&amp;luga:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="219"/>
+        <source>&amp;Path:</source>
+        <translation>&amp;Staza:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="232"/>
+        <source>&amp;Interface:</source>
+        <translation>S&amp;učelje:</translation>
+    </message>
+    <message>
+        <location filename="../edit_action_dialog.ui" line="245"/>
+        <source>&amp;Method:</source>
+        <translation>&amp;Metoda:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../main_window.ui" line="14"/>
+        <source>Global Actions Manager</source>
+        <translation>Upravljanje globalnim radnjama</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="55"/>
+        <source>Add ...</source>
+        <translation>Dodaj …</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="65"/>
+        <source>Remove</source>
+        <translation>Ukloni</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="75"/>
+        <source>Modify ...</source>
+        <translation>Promijeni …</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="92"/>
+        <source>Swap</source>
+        <translation>Zamijeni</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="118"/>
+        <source>Multiple actions behaviour:</source>
+        <translation>Redoslijed izvršavanja radnji:</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="126"/>
+        <source>First</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished">Prva</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="131"/>
+        <source>Last</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished">Zadnja</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="136"/>
+        <source>None</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished">Nijedna</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="141"/>
+        <source>All</source>
+        <comment>globalkeys/config/multipleActions</comment>
+        <translation type="unfinished">Sve</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="166"/>
+        <source>Default</source>
+        <translation>Standardno</translation>
+    </message>
+    <message>
+        <location filename="../main_window.ui" line="186"/>
+        <source>Close</source>
+        <translation>Zatvori</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="187"/>
+        <source>Restore Default</source>
+        <translation>Vrati standardne postavke</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="188"/>
+        <source>Your defined shortcuts will be removed.
+Do you want to proceed?</source>
+        <translation>Tvoji prečaci će se ukloniti.
+Želiš li nastaviti?</translation>
+    </message>
+</context>
+</TS>
